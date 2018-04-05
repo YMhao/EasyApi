05db3136f7beb85ef3078b48c38bfe47c5d576b4
yu <yu@759290728@qq.com>
2018-04-05 22:00:30 +0800

    update readme and update helloword demo

--- examples/helloworld/main.go
+++ examples/helloworld/main.go
@@ -4,6 +4,34 @@ import (
 	"github.com/YMhao/EasyApi/serv"
 )
 
+type HelloRequest struct {
+	Name string `desc:"The request message containing the user's name."`
+}
+
+type HelloRespone struct {
+	Message string `desc:"The response message containing the greetings"`
+}
+
+var HelloAPI = serv.NewAPI(
+	"helloWord",
+	`api for helloword`,
+	&HelloRequest{},
+	&HelloRespone{},
+	HelloAPICall,
+)
+
+func HelloAPICall(data []byte) (interface{}, *serv.APIError) {
+	req := &HelloRequest{}
+	err := serv.UnmarshalAndCheckValue(data, req)
+	if err != nil {
+		return nil, serv.NewError(err)
+	}
+
+	return &HelloRespone{
+		Message: "hello " + req.Name + "!",
+	}, nil
+}
+
 func main() {
 	conf := serv.NewAPIServConf("1.0", "", "helloWorld", "EasyApi 的 hello World")
 	conf.DebugOn = true
